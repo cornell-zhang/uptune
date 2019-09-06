2902d8dcc5f20f40ab748286ea22d481686f78dc
CUNXI YU <ycunxi@umass.edu>
2019-09-06 11:30:42 -0600

    ABC example update using new UI; bug report

--- examples/abc/README.md
+++ examples/abc/README.md
@@ -1,15 +1,17 @@
-Exmaple Single Stage
+Tuning ABC (logic synthesis) synthesis flow - Exmaple Single Stage
 ====================
 
-#### How to run
+#### Bug report
 
-tune design for 20 epoches (intrusive mode)  
-``` shell
-uptune single.py -pf 2 --test-limit 20
 ```
-
-tune design for 2000 seconds (template mode)
-``` shell
-uptune single_tpl.py --timeout 2000
+python -m uptune.on abc.py -pf 1 --time_out 360
 ```
+Traceback (most recent call last):
+  File "/usr/local/Cellar/python/3.7.4/Frameworks/Python.framework/Versions/3.7/lib/python3.7/runpy.py", line 193, in _run_module_as_main
+    "__main__", mod_spec)
+  File "/usr/local/Cellar/python/3.7.4/Frameworks/Python.framework/Versions/3.7/lib/python3.7/runpy.py", line 85, in _run_code
+    exec(code, run_globals)
+  File "/Users/cunxiyu/Desktop/Research_Group/tools/uptune/uptune/on.py", line 144, in <module>
+    on.main()
+AttributeError: 'int' object has no attribute 'main'
 
--- examples/abc/abc.py
+++ examples/abc/abc.py
@@ -1,36 +1,37 @@
-from uptune import autotune, feedback
+import uptune as ut
 import random
 import subprocess
 import re
 
 #resub -K 6 -l 2
-
+aig_pth = "i10.aig"
 abc_opt_pool = ['balance', 'rewrite', 'resub', 'refactor', 'rewrite -z', 'refactor -z']
 syn_all = []
 for i in range(0,24):
-    at = autotune(abc_opt_pool[random.randint(0,5)], abc_opt_pool)
-    at2 = autotune(6, [6,8,10,12])
+    ut_idx0 = ut.tune(0, (0, 5), name='int_range')
+    at = abc_opt_pool[ut_idx0]
+    at2 = ut.tune(6, [6,8,10,12])
     if at == 'resub':
         at += ' -K ' + str(at2)
     syn_all.append(at)
 
-def abc_synthesis_flow():
-    command = "abc -c \"read i10.aig;"
+def abc_synthesis_flow(aig_pth="i10.aig"):
+    command = "abc -c \"read %s;" %aig_pth
     for at in syn_all:
         command += at+";"
     command +="if -K 6;print_stats\" > abc_if_res.log"
     return command
 
-print(abc_synthesis_flow())
+print(abc_synthesis_flow()[:-16])
 process = subprocess.Popen(abc_synthesis_flow(), shell=True)
 process.wait()
 def res():
     with open("abc_if_res.log", 'rb') as f:
         l = f.readlines()[-1]
     f.close()
-    print(str(l))
+    #print(str(l))
     x = re.findall("\d+", str(l))
+    print("%s: #LUT = %d ; #LEV = %d" % (aig_pth, int(x[9]), int(x[-1])))
     return int(x[9]), int(x[-1]) # lut count, depth
 
-
-val = feedback(res()[0])
+val = ut.target(res()[0],objective='min')
